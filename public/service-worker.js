70b612ba7b667106d5162ff8fc7d10acabb3e7e8
Darren Watson <v_tec@live.com>
2021-08-23 03:31:32 -0500

    renamed a var but didnt change it elsewhere no depoly should work

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
       caches.open(CACHE_NAME).then(function(cache) {
         console.log("Opened cache");
-        return cache.addAll(urlsToCache);
+        return cache.addAll(FILES_TO_CACHE);
       })
     );
   });
