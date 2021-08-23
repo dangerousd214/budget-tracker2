b7a74713223c04238b4adf393d6ee55af50f57a7
Darren Watson <v_tec@live.com>
2021-08-23 02:55:07 -0500

    forgot to add routes to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
